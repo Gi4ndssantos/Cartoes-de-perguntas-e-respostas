7eb113e158711fe1875941b76a4e9c63105bd57c
Gi4ndssantos <162325102+Gi4ndssantos@users.noreply.github.com>
2024-10-29 09:35:53 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -7,17 +7,17 @@ criaCartao(
 criaCartao(
     'Reciclagem',
     'Que tipo de lixo deve ser jogado na lixeira verde?',
-    'A capital da França é Paris'
+    'O vidro'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Reciclagem',
+    'Que tipo de lixo deve ser jogado na lixeira vermelha?',
+    'O plástico'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Reciclagem',
+    'Que tipo de lixo deve ser jogado na lixeira amarela?',
+    'O metal'
 )
